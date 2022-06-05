8e036e72e0faf75be15fe13907ff4542b7c38293
pedved <somichandriicz@gmail.com>
2022-06-05 16:56:05 +0200

    Добавьте файлы проекта.

---
+++ Hello_Cons_Dr_Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(10, 12, 14, 16, '*', "hi girls");
+            box.draw();
+
+            try
+            {
+                Console.WriteLine("Press any key...");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!");
+            }
+
+        }
+    }
+}
